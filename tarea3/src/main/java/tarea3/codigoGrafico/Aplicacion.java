2be7c616baa5382feab9294b1d8f8564556edc64
María Jesús <marolivares2023@udec.cl>
2024-05-29 12:22:11 -0400

    feat: Aplicacion
    la aplicacion actua como main, para hacer visible la ventana

---
+++ tarea3/src/main/java/tarea3/codigoGrafico/Aplicacion.java
@@ -0,0 +1,12 @@
+package tarea3.codigoGrafico;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+    
+}
