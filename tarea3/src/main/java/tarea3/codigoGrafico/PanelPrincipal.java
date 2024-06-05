b1a14302e9bfd8acf9c9e82aabd52a6deedf7742
María Jesús <marolivares2023@udec.cl>
2024-06-04 21:49:11 -0400

    fix: vista de los productos

--- tarea3/src/main/java/tarea3/codigoGrafico/PanelPrincipal.java
+++ tarea3/src/main/java/tarea3/codigoGrafico/PanelPrincipal.java
@@ -6,14 +6,14 @@ import java.awt.*;
 public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
     private PanelComprador com;
     private PanelExpendedor exp;
-    JPanel panelPrincipal = new JPanel();
   
     public PanelPrincipal () {
         exp = new PanelExpendedor();
-        this.setBackground(Color.white);
         com = new PanelComprador();
         this.add(exp);
         this.add(com);
+        setOpaque(false);
+
     }
     @Override
     public void paintComponent (Graphics g) { //todo se dibuja a partir de este método
--- tarea3/src/main/java/tarea3/codigoGrafico/PanelProductos.java
+++ tarea3/src/main/java/tarea3/codigoGrafico/PanelProductos.java
@@ -35,8 +35,8 @@ public class PanelProductos extends JPanel {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        int x1 = 450;
-        int x2 = 580;
+        int x1 = 455;
+        int x2 = 590;
         int x3 = x1;
         int x4 = x2;
         int x5 = x1;
@@ -44,30 +44,56 @@ public class PanelProductos extends JPanel {
         for(int i = 0; i < 6 ;i++){
             if(Coca.size() > i){
                 Image image = Coca.getP(i).getImage();
-                g.drawImage(image, x1, 159, 60, 60, this);
+                g.drawImage(image, x1, 155, 60, 60, this);
                 x1 += 20;
             }
             if(Sprite.size() > i){
                 Image image = Sprite.getP(i).getImage();
-                g.drawImage(image, x2, 159, 60, 60, this);
+                g.drawImage(image, x2, 155, 60, 60, this);
                 x2 += 20;
             }
             if(Fanta.size() > i){
                 Image image = Fanta.getP(i).getImage();
-                g.drawImage(image, x3, 229, 60, 60, this);
+                g.drawImage(image, x3, 225, 60, 60, this);
                 x3 += 20;
             }
            if(Snickers.size() > i){
                 Image image = Snickers.getP(i).getImage();
-                g.drawImage(image, x4, 229, 60, 60, this);
+                g.drawImage(image, x4, 225, 60, 60, this);
                 x4 += 20;
             }
             if(Super8.size() > i){
                 Image image = Super8.getP(i).getImage();
-                g.drawImage(image, x5, 300, 60, 60, this);
+                g.drawImage(image, x5, 296, 60, 60, this);
                 x5 += 20;
             }
         }
         
+    }
+    public void getImagenProducto(int num){
+        switch (num) {
+            case 1:
+                Coca.get();
+                break;
+            
+            case 2:
+                Sprite.get();
+                break;
+
+            case 3:
+                Fanta.get();
+                break;
+
+            case 4:
+                Snickers.get();
+                break;
+
+            case 5:
+                Super8.get();
+                break;
+
+            default:
+                break;
+        }
     }
 }
