c0f21408fc152b344c5bc5ca9e6d93429e063032
Arcisternas <arcisternas2023@udec.cl>
2024-06-04 20:09:40 -0400

    feat: Panel Comprador
    -Se define panel comprador

--- tarea3/src/main/java/tarea3/codigoGrafico/PanelComprador.java
+++ tarea3/src/main/java/tarea3/codigoGrafico/PanelComprador.java
@@ -6,4 +6,19 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 public class PanelComprador extends JPanel{
+    private JPanel panelComprador = new JPanel();
+    private JLabel Precios;
+    private Image titulo;
+    public PanelComprador(){
+        //this.setSize(350, 800);
+        //this.setBackground(Color.CYAN);
+        titulo = new ImageIcon(getClass().getResource("/imagenes/ventana/clean_customer_prices.png")).getImage();
+        setOpaque(false);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(titulo, 0, 0, getWidth(), getHeight(), this);
+    }
 }
